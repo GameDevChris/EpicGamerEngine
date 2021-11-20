e0ce532950e2c23e6f624d43f16d4ae3792df66d
Chris <chris@campfiregames.co.uk>
2021-11-20 14:17:59 +0000

    Working on bot

--- EngineBot/EngineBot.cpp
+++ EngineBot/EngineBot.cpp
@@ -2,33 +2,11 @@
 #include <iostream>  
 #include <sstream>  
 #include <vector>
-#include <windows.h>
-#include <urlmon.h>
+
 
 using namespace std;
+dpp::snowflake* globalChannel;
 
-void GetFile()
-{
-	LPCTSTR szUrl = (LPCTSTR)("https://gist.githubusercontent.com/CampfireGames-Chris/f0dd268fd097c56b3557d4347b0ee48e/raw/6db43ac4d139b13e38c3a6a8e8ac3f13ed72c807/scoreData.txt");
-
-	LPCTSTR szOutput = (LPCTSTR)("../downloadedScoreData.txt");
-
-	HRESULT result = URLDownloadToFile(nullptr, szUrl, szOutput, 0, nullptr);
-
-	switch (result)
-	{
-	case S_OK:
-		cout << "Download ok.";
-		break;
-	case E_OUTOFMEMORY:
-		cout << "The buffer length is invalid, or there is insufficient memory to complete the operation.";
-		break;
-	case INET_E_DOWNLOAD_FAILURE:
-		cout << "The specified resource or callback interface was invalid.";
-		break;
-	}
-
-}
 
 void PrintAllScores(dpp::cluster* bot, dpp::snowflake* channel)
 {
@@ -132,22 +110,100 @@ void PrintHighScores(dpp::cluster* bot, dpp::snowflake* channel)
 	bot->message_create(dpp::message(*channel, scoreText));
 }
 
+void PrintNewestScore(dpp::cluster* bot, dpp::snowflake* channel)
+{
+	vector<int>* dataVector = new vector<int>;
+	dataVector->clear();
+
+	string filename("../scoreData.txt");
+	int number;
+	ifstream input_file(filename);
+
+	while (input_file >> number) {
+		dataVector->push_back(number);
+	}
+
+	cout << endl;
+	input_file.close();
+
+	bot->message_create(dpp::message(*channel, "New score:"));
+	bot->message_create(dpp::message(*channel, "-----"));
+
+	int val = (*dataVector)[dataVector->size()];
+	stringstream ss;
+
+	ss << val;
+	std::string scoreText;
+	ss >> scoreText;
+
+	bot->message_create(dpp::message(*channel, scoreText));
+}
+
+std::string GetJob()
+{
+	std::string job = "";
+
+	string filename("../jobData.txt");
+
+	ifstream input_file(filename);
+
+	if (input_file >> job)
+	{
+		return job;
+		input_file.close();
+	}
+
+	else
+	{
+		return "";
+		input_file.close();
+	}
+}
+
+void doJob(dpp::cluster* bot, dpp::snowflake* channel)
+{
+	std::string myJob = GetJob();
+
+	if (myJob == "newScore")
+	{
+		PrintNewestScore(bot, channel);
+	}
+
+	else
+	{
+		cout << "No job for me" << endl;
+	}
+}
+
 int main()
 {
-	GetFile();
-
 	dpp::cluster bot("OTExNDEzMzQwOTI3MDQ1NzI2.YZhB0Q.Ug6ghF6e1bf62D7W4c3g7kTVUBI");
 
 	bot.on_ready([&bot](const dpp::ready_t& event)
 		{
 			std::cout << "Logged in as " << bot.me.username << "!\n";
+
+			dpp::snowflake myChannel = dpp::snowflake("838441750611230723");
+
+			bot.message_create(dpp::message(myChannel, "Am alive!"));
+
+			//doJob(&bot, &myChannel);
 		});
 
 	bot.on_message_create([&bot](const dpp::message_create_t& event)
 		{
 			if (event.msg->content == "!scores")
 			{
+				cout << event.msg->channel_id << endl;
 				PrintAllScores(&bot, &(event.msg->channel_id));
+			}
+
+			else if (event.msg->content == "!getChannel")
+			{
+				cout << event.msg->channel_id << endl;
+				globalChannel = &event.msg->channel_id;
+
+				bot.message_create(dpp::message(*globalChannel, "Am alive!"));
 			}
 
 			else if (event.msg->content == "!top5")
